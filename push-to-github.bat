7f71f9637286b7529b1dd8f65aa2edab62ea5b14
Muhammad Abdullah Wali <abdullahwali79@gmail.com>
2025-10-27 10:29:12 +0500

    submit

---
+++ push-to-github.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Pushing changes to GitHub...
+git config --global user.name "cryptohawke01-ui"
+git config --global user.email "cryptohawke01-ui@users.noreply.github.com"
+git push origin main
+pause
